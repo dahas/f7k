7aebc46946a216578dba99cb71d7a7347f2e26df
dahas <martin.j.wolf@gmail.com>
2023-03-31 08:16:53 +0200

    Removed JsonAdapter

--- controller/ExampleJsonController.php
+++ controller/ExampleJsonController.php
@@ -2,25 +2,28 @@
 
 namespace f7k\Controller;
 
-use f7k\Library\JsonAdapter;
-use f7k\Sources\attributes\{Inject, Route};
+use f7k\Sources\attributes\Route;
 use f7k\Sources\{Request, Response};
 
 class ExampleJsonController extends AppController {
 
-    #[Inject(JsonAdapter::class)]
-    protected $json;
-    
     #[Route(path: '/ExampleJson', method: 'get')]
     public function main(Request $request, Response $response): void
     {
-        $this->json->setMessage("Success");
-        $this->json->setData([
+        $data = [
             ["id" => 1, "manufacturer" => "Ferrari", "year" => 1958],
             ["id" => 2, "manufacturer" => "Lotus", "year" => 1973],
             ["id" => 3, "manufacturer" => "Mercedes Benz", "year" => 2012],
+        ];
+
+        $json = json_encode([
+            "message" => "Success",
+            "url" => "http://localhost:2400/ExampleJson",
+            "data" => $data,
+            "count" => count($data),
         ]);
-        $this->json->addField("url", "http://localhost:2400/ExampleJson");
-        $this->json->encode($request, $response);
+
+        $response->addHeader("Content-Type", "application/json");
+        $response->write($json);
     }
 }
