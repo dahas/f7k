67541e7a474e7af93a3f3286592ef667941a4d49
dahas <martin.j.wolf@gmail.com>
2023-04-02 14:33:45 +0200

    Refactoring

--- src/Application.php
+++ src/Application.php
@@ -3,29 +3,24 @@
 namespace f7k\Sources;
 
 use f7k\Sources\interfaces\AppInterface;
-use f7k\Controller\NotFoundController;
 use f7k\Sources\Request;
 use f7k\Sources\Response;
 use f7k\Sources\Router;
 
 class Application implements AppInterface {
 
-    protected Request $request;
-    protected Response $response;
-    protected Router $router;
-
     public function execute(): void
     {
-        $this->request = new Request();
-        $this->response = new Response();
+        $request = new Request();
+        $response = new Response();
         
-        $this->router = new Router($this->request, $this->response);
-        $this->router->notFound(function() {
+        $router = new Router($request, $response);
+        $router->notFound(function() {
             header("location: /PageNotFound");
             exit();
         });
-        $this->router->run();
+        $router->run();
 
-        $this->response->flush();
+        $response->flush();
     }
 }
