8b77cbc7eca3ebdaba13d5e4494b88556c8f6ea5
dahas <martin.j.wolf@gmail.com>
2023-04-05 08:44:35 +0200

    Throws Exception If Key Doesnt Consist Of Letters

--- src/exceptions/InvalidArgumentException.php
+++ src/exceptions/InvalidArgumentException.php
@@ -4,17 +4,11 @@ namespace f7k\Sources\exceptions;
 
 use f7k\Sources\interfaces\InvalidArgumentExceptionInterface;
 
-/**
- * MUST be thrown if the $key string is not a legal value.
- */
+
 class InvalidArgumentException extends \Exception implements InvalidArgumentExceptionInterface {
 
-    public function __construct($message = "", $code = 0, \Throwable $previous = null)
+    public function __construct($message, $code = 0, \Throwable $previous = null)
     {
-        if (!$message) {
-            $message = "\$key string is not a legal value.";
-        }
-
         parent::__construct($message, $code, $previous);
     }
 
