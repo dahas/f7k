a10825ed996f89722138843d6db80bf496877f41
dahas <martin.j.wolf@gmail.com>
2023-04-18 12:20:42 +0200

    Added Session Manager

--- src/Session.php
+++ src/Session.php
@@ -6,6 +6,11 @@ use f7k\Sources\interfaces\SessionInterface;
 
 class Session implements SessionInterface {
 
+    public function start(): void
+    {
+        session_start();
+    }
+
     public function get(string $name): mixed
     {
         return $_SESSION[$name];
@@ -16,12 +21,28 @@ class Session implements SessionInterface {
         $_SESSION[$name] = $value;
     }
 
-    public function storeTemporay(mixed $temp): void
+    public function unset(string $name): void
+    {
+        unset($_SESSION[$name]);
+    }
+
+    public function getTemp(): mixed
+    {
+        return $_SESSION['temp'] ?? null;
+    }
+
+    public function setTemp(mixed $temp): void
     {
         $_SESSION['temp'] = $temp;
     }
 
+    public function unsetTemp(): void
+    {
+        unset($_SESSION['temp']);
+    }
+
     public function destroy(): void
     {
+        session_destroy();
     }
 }
--- src/interfaces/SessionInterface.php
+++ src/interfaces/SessionInterface.php
@@ -4,7 +4,9 @@ namespace f7k\Sources\interfaces;
 
 interface SessionInterface {
 
+    public function start(): void;
     public function get(string $name): mixed;
     public function set(string $name, mixed $value): void;
+    public function unset(string $name, mixed $value): void;
     public function destroy(): void;
 }
